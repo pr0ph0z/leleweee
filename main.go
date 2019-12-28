f31eda5efd3ed46aae6209fd8263553b1c0d9637
Mohamad Radisha <pr0ph0z23@gmail.com>
2019-12-28 23:08:36 +0700

    Right concept but there are some things that missed

--- main.go
+++ main.go
@@ -27,8 +27,10 @@ func main() {
 		if len(mentions) != 0 {
 			sinceID = mentions[0].ID
 			for _, mention := range mentions {
+				var userTweet, _, _ = client.Statuses.Show(mention.InReplyToStatusID, nil)
+
 				var userPattern = regexp.MustCompile(`(@[a-zA-Z0-9_]{0,}\s)`)
-				var tweet = userPattern.ReplaceAllString(mention.Text, "")
+				var tweet = userPattern.ReplaceAllString(userTweet.Text, "")
 				var vowelPattern = regexp.MustCompile(`[aiueo]`)
 				var replaceTweet = vowelPattern.ReplaceAllString(tweet, "i")
 
@@ -36,7 +38,6 @@ func main() {
 				_, _, _ = client.Statuses.Update("@"+mention.User.ScreenName+" "+replaceTweet, statusUpdateParams)
 			}
 		}
-
 		time.Sleep(30 * time.Second)
 	}
 }
