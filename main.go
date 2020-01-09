621c1a1f85f133fde0ac4e6ede747d2e39d2106a
Mohamad Radisha <pr0ph0z23@gmail.com>
2020-01-09 15:55:26 +0700

    Add filter tweet using hashtag, filter URL

--- main.go
+++ main.go
@@ -27,15 +27,20 @@ func main() {
 		if len(mentions) != 0 {
 			sinceID = mentions[0].ID
 			for _, mention := range mentions {
-				var userTweet, _, _ = client.Statuses.Show(mention.InReplyToStatusID, nil)
+				var matchIdentifier, _ = regexp.MatchString(`#keun`, mention.Text)
+				if matchIdentifier {
+					var userTweet, _, _ = client.Statuses.Show(mention.InReplyToStatusID, nil)
 
-				var userPattern = regexp.MustCompile(`(@[a-zA-Z0-9_]{0,}\s)`)
-				var tweet = userPattern.ReplaceAllString(userTweet.Text, "")
-				var vowelPattern = regexp.MustCompile(`[aiueo]`)
-				var replaceTweet = vowelPattern.ReplaceAllString(tweet, "i")
+					var userPattern = regexp.MustCompile(`(@[a-zA-Z0-9_]{0,}\s?)`)
+					var userRegexTweet = userPattern.ReplaceAllString(userTweet.Text, "")
+					var urlPattern = regexp.MustCompile(`\s?https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)`)
+					var urlRegexTweet = urlPattern.ReplaceAllString(userRegexTweet, "")
+					var vowelPattern = regexp.MustCompile(`[aiueo]`)
+					var replaceTweet = vowelPattern.ReplaceAllString(urlRegexTweet, "i")
 
-				statusUpdateParams := &twitter.StatusUpdateParams{InReplyToStatusID: mention.ID}
-				_, _, _ = client.Statuses.Update("@"+mention.User.ScreenName+" "+replaceTweet, statusUpdateParams)
+					statusUpdateParams := &twitter.StatusUpdateParams{InReplyToStatusID: mention.ID}
+					_, _, _ = client.Statuses.Update("@"+mention.User.ScreenName+" "+replaceTweet, statusUpdateParams)
+				}
 			}
 		}
 		time.Sleep(30 * time.Second)
